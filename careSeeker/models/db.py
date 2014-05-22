25d3b95ae93fb842c96c4069cf6d4215555dab91
Daniel Esposito <danielce90@gmail.com>
2014-05-22 18:40:26 +1000

    Added mail sender
    To do:
    
    Search
    Schedule
    Settings
    Bugtesting

---
+++ careSeeker/models/db.py
@@ -0,0 +1,163 @@
+# -*- coding: utf-8 -*-
+
+#########################################################################
+## This scaffolding model makes your app work on Google App Engine too
+## File is released under public domain and you can use without limitations
+#########################################################################
+
+## if SSL/HTTPS is properly configured and you want all HTTP requests to
+## be redirected to HTTPS, uncomment the line below:
+# request.requires_https()
+
+if not request.env.web2py_runtime_gae:
+    ## if NOT running on Google App Engine use SQLite or other DB
+    db = DAL('sqlite://storage.sqlite',pool_size=1,check_reserved=['all'])
+else:
+    ## connect to Google BigTable (optional 'google:datastore://namespace')
+    db = DAL('google:datastore')
+    ## store sessions and tickets there
+    session.connect(request, response, db=db)
+    ## or store session in Memcache, Redis, etc.
+    ## from gluon.contrib.memdb import MEMDB
+    ## from google.appengine.api.memcache import Client
+    ## session.connect(request, response, db = MEMDB(Client()))
+
+## by default give a view/generic.extension to all actions from localhost
+## none otherwise. a pattern can be 'controller/function.extension'
+response.generic_patterns = ['*'] if request.is_local else []
+## (optional) optimize handling of static files
+# response.optimize_css = 'concat,minify,inline'
+# response.optimize_js = 'concat,minify,inline'
+## (optional) static assets folder versioning
+# response.static_version = '0.0.0'
+#########################################################################
+## Here is sample code if you need for
+## - email capabilities
+## - authentication (registration, login, logout, ... )
+## - authorization (role based authorization)
+## - services (xml, csv, json, xmlrpc, jsonrpc, amf, rss)
+## - old style crud actions
+## (more options discussed in gluon/tools.py)
+#########################################################################
+TYPE=[
+                   T('Carer'),
+                   T('Care seeker'),
+                   T('other')
+                ]
+gender=[
+T('Male'),
+T('Female'),
+T('Other')
+]
+from gluon.tools import Auth, Crud, Service, PluginManager, prettydate
+auth = Auth(db)
+crud, service, plugins = Crud(db), Service(), PluginManager()
+auth.settings.extra_fields['auth_user']= [
+Field('Phone', 'integer', requires=IS_NOT_EMPTY()),
+    Field('Age', 'integer'),
+    Field('Gender', 'string',requires=IS_IN_SET(gender,zero=T('--Please select--'))),
+    Field('Profile_Pict','upload'),
+    Field('User_Type', 'string',requires=IS_IN_SET(TYPE, labels=['Looking for a job', 'Looking for a carer', 'other'],zero=T('--choose your purpose--')))]
+## create all tables needed by auth if not custom tables
+auth.define_tables(username=True, signature=False)
+
+## configure email
+mail = auth.settings.mailer
+mail.settings.server = 'logging' or 'smtp.gmail.com:587'
+mail.settings.sender = 'nhimbong94@gmail.com'
+mail.settings.login = 'username:password'
+
+## configure auth policy
+auth.settings.registration_requires_verification = False
+auth.settings.registration_requires_approval = False
+auth.settings.reset_password_requires_verification = True
+## if you need to use OpenID, Facebook, MySpace, Twitter, Linkedin, etc.
+## register with janrain.com, write your domain:api_key in private/janrain.key
+from gluon.contrib.login_methods.rpx_account import use_janrain
+use_janrain(auth, filename='private/janrain.key')
+
+#########################################################################
+## Define your tables below (or better in another model file) for example
+##
+## >>> db.define_table('mytable',Field('myfield','string'))
+##
+## Fields can be 'string','text','password','integer','double','boolean'
+##       'date','time','datetime','blob','upload', 'reference TABLENAME'
+## There is an implicit 'id integer autoincrement' field
+## Consult manual for more options, validators, etc.
+##
+## More API examples for controllers:
+##
+## >>> db.mytable.insert(myfield='value')
+## >>> rows=db(db.mytable.myfield=='value').select(db.mytable.ALL)
+## >>> for row in rows: print row.id, row.myfield
+#########################################################################
+db.define_table('WorkWith',
+    Field('Carer', 'reference auth_user'),
+    Field('CareSeeker', 'reference auth_user')
+    )
+## use this table for both requirements and experience => easier to search
+db.define_table('ExperienceRequirementType',
+    Field('ExperienceType', 'string')# we can use this as dropdown box options
+    )
+db.define_table('Experience',
+    Field('ExperienceType', 'reference ExperienceRequirementType'),
+    Field('UserID', 'reference auth_user'),
+    Field('Qualification', 'string'),
+    Field('Period', 'integer'), # in months
+    Field('Details','text')
+    )
+
+
+db.define_table('Requirement',
+    Field('RequirementType', 'reference ExperienceRequirementType'),
+    Field('MyUserID', 'reference auth_user'),
+    Field('Gender', 'string'),
+    Field('Qualification', 'string'),
+    Field('Period', 'integer'),# in months
+    Field('Availability', 'integer')# in hours
+    )
+db.define_table('MyMessage',
+    Field('FromUser', 'reference auth_user'),
+    Field('ToUser', 'reference auth_user'),
+    Field('MessageDate', 'datetime', default=request.now),
+    Field('Detail', 'text')
+    )
+db.define_table('Address',
+    Field('MyUserID', 'reference auth_user'),
+    Field('StreetNumber', 'integer'),
+    Field('Street', 'string'),
+    Field('Postcode', 'integer'),
+    Field('Area', 'string'),
+    Field('City', 'string')
+    )
+db.define_table('Schedule',
+    Field('MyUserID', 'reference auth_user'),
+    Field('Days', 'string'),# we can hard code the days
+    Field('HourFrom', 'integer'), # we can hard code these too
+    Field('HourTo', 'integer')
+    )
+db.define_table('Bookings',
+    Field('ScheduleID', 'reference Schedule'),
+    Field('CareSeeker', 'reference auth_user'),
+    Field('HourFrom', 'integer'),# we don't need day as it is defined in Schedule already
+    Field('HourTo', 'integer')
+    )
+db.define_table('UserRating',
+   Field('CareSeeker', 'reference auth_user'),
+   Field('Carer', 'reference auth_user'),
+   Field('RatingTime', 'datetime')
+   )
+db.define_table('RatingMetric',
+   Field('Criteria', 'string'),
+   Field('DataInputType', 'string')
+   )
+db.define_table('Scores',
+   Field('URID', 'reference UserRating'),
+   Field('RMID', 'reference RatingMetric'),
+   Field('Score', 'integer')
+   )
+
+
+#enable auditing
+auth.enable_record_versioning(db)
